4c3fb443a1eb306409f7944cade4bdd9f3dbc8f1
Thierry-E <76159642+Thierry-E@users.noreply.github.com>
2024-04-23 12:14:06 +0200

    Modification des images de la galerie et de cette dernière via les fichiers scss et js  pour inclure une modale, correction d'une erreur de syntawx dans le fichier projets.json.

--- assets/scripts/gallery.js
+++ assets/scripts/gallery.js
@@ -1,11 +1,11 @@
+//Import du fichier json "projets"
+const projets = './assets/datas/projets.json'
+
 //Variables Globales
 
 // Récupération de la div gallery dans le DOM (page web)
 const gallery = document.querySelector('.gallery')
 //console.log(gallery)
-
-//Import du fichier json "projets"
-const projets = './assets/datas/projets.json'
 
 fetch(projets)
   .then((response) => {
@@ -21,29 +21,59 @@ fetch(projets)
       const cards = document.createElement('div')
       cards.classList.add('cards')
 
-      //Création de la div "infos" contenant les infos du projet.
-      const infos = document.createElement('div')
-      infos.classList.add('infos')
-
-      //Création de la div "picture" contenant l'image et le lien du projet.
-      const picture = document.createElement('div')
-      picture.classList.add('picture')
-
-      //Ajout des données des projets.
-      infos.innerHTML = `
-        <h3>${projet.name}</h3>
-        <p>${projet.description}</p>
-        <p>${projet.langages}</p>
+      // Ajout des données à la div "cards"
+      cards.innerHTML = `
+      <img src=${projet.picture} alt=${projet.name}>
+      <h3>${projet.name}</h3>
       `
-      picture.innerHTML = ` <img src="${projet.picture}" alt="${projet.name}">
-        <a href="${projet.url}">${projet.name}</a>`
+
+      //Ajout d'un écouteur d'événement sur la div "cards"
+      cards.addEventListener('click', () => {
+        openModal(projet)
+      })
 
       //Ajout de la div "cards comme enfant à la div "gallery"
       gallery.appendChild(cards)
-      cards.appendChild(infos)
-      cards.appendChild(picture)
     })
   })
   .catch((error) => {
     console.error('Erreur:', error)
   })
+
+// Fonction pour ouvrir la modale comportant les infos du projet
+function openModal(projet) {
+  // Création de l'overlay
+  const overlay = document.createElement('div')
+  overlay.classList.add('overlay')
+
+  //Création de la modale
+  const modal = document.createElement('div')
+  modal.classList.add('modal')
+
+  //Ajout du contenu à la modale
+  modal.innerHTML = `
+  <i class="fa-solid fa-xmark"></i>
+  <img src=${projet.picture} alt=${projet.name}>
+  <h2>${projet.name}</h2>
+  <p>${projet.description}</p>
+  <p>${projet.langages}</p>
+  <a href=${projet.url} target="_blank">Voir le site</a>
+  `
+
+  // ajout de la modal à la galerie
+  gallery.appendChild(overlay)
+  gallery.appendChild(modal)
+
+  // Ajout d'un écouteur d'événement pour fermer la modale avec au clic sur la croix.
+  const close = document.querySelector('.fa-xmark')
+  close.addEventListener('click', () => {
+    modal.remove() //Supprimer la modale
+    overlay.remove() //supprimer la modale
+  })
+
+  // Ajout d'un écouteur d'événement pour fermer la modale au clic sur l'overlay
+  overlay.addEventListener('click', () => {
+    modal.remove()
+    overlay.remove()
+  })
+}
